102c0eccec7a9941cbc7237db8a50e6e261d71a1
Julien Turby <julien.turby@hellocasa.fr>
2018-10-05 16:49:33 +0200

    format json

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-
-function format() {
-    var jsonParsed = JSON.parse(document.getElementById('content').value);
-    document.getElementById('test').textContent = jsonParsed;
-}
-document.getElementById('format-json').onclick = format;
+let formatBtn = document.getElementById('formatJson');
+formatBtn.onclick = function() {
+    try {
+        var jsonParsed = JSON.parse(document.getElementById('content').value);
+        document.getElementById('output').textContent = JSON.stringify(jsonParsed, null, 4);
+    }
+    catch(error) {
+        document.getElementById('output').textContent = error;
+    }
+};
